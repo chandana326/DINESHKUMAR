6abe3f71067c4caf6a9192c0dc1bc5a17b193bb2
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-25 10:26:30 +0530

    Update and rename day7 Q1 little endian to big endian.txt to day7 Q1 little endian to big endian.c

--- WEEK2/DAY7/day7 Q1 little endian to big endian.c
+++ WEEK2/DAY7/day7 Q1 little endian to big endian.c
@@ -4,17 +4,21 @@ LITTLE ENDEN TO BIG ENDEN ALGORITHM
 
 */
 STEP 1:
-checking whether the system fallows little or big endian
+1.checking whether the system fallows little or big endian
+2.converting nibble ordering 
+3.print resultafter converting
 #include <stdio.h>
 
 int main() {
     // Write C code here
-   char num;
+   char num=1;
    printf("enter a number:\n");
    scanf("%d",&num);
    char *ptr=&num;
-   if(*ptr==num)
+   if(*ptr==num){
    printf("LITTILE ENDIAN\n");
+    printf("%d\n",(num>>4|num<<4));
+   }
    printf("we can't convert CPU byte order from littile endian to big endian");
     return 0;
 }
